1b7bc01382a603065c6156404b828d23b05464ff
Amit Singh <https://github.com/AutomationAmitSingh/JavaPluralExamples.git>
2025-04-15 20:12:25 +0530

    added testcases execution and update code

--- src/test/java/serenity/AcceptanceTestSuite.java
+++ src/test/java/serenity/AcceptanceTestSuite.java
@@ -1,8 +1,22 @@
 package serenity;
 
+import logger.Log;
+import manageTestCases.ZephyrCloudConnector;
+import net.serenitybdd.core.Serenity;
 import net.serenitybdd.jbehave.SerenityStories;
+import net.thucydides.core.model.TestOutcome;
+import net.thucydides.core.steps.StepEventBus;
+import net.thucydides.core.util.SystemEnvironmentVariables;
+import org.jbehave.core.annotations.AfterScenario;
 import org.jbehave.core.annotations.BeforeStories;
+import org.jbehave.core.annotations.ScenarioType;
 import utils.JsonUtils;
+
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
 public class AcceptanceTestSuite extends SerenityStories {
 
@@ -24,6 +38,35 @@ public class AcceptanceTestSuite extends SerenityStories {
         }catch (Exception e)
         {
             System.out.println("Story file name not found running through meta filter only!:-->"+e.getMessage());
+        }
+    }
+
+    @AfterScenario(uponType = ScenarioType.EXAMPLE)
+    public void executeTestsAndMarkStatus() throws URISyntaxException {
+        String connectionFlag = SystemEnvironmentVariables.createEnvironmentVariables().getProperty("zephyr.connectionFlag");
+        Log.info("Connection flag for execute and mark testcases status is: "+connectionFlag);
+        if(connectionFlag.toUpperCase().equals("YES"))
+        {
+            Log.info("<--------Start of marking the testcases status in Zephyr-------->");
+            Map <String, String> metaDataMap = Serenity.getCurrentSession().getMetaData();
+            List <String> testCaseNameList = new ArrayList<String>();
+            List <String> testCaseIssueKeyList = new ArrayList<String>();
+            for(Map.Entry<String, String> entry : metaDataMap.entrySet())
+            {
+               if(entry.getKey().equalsIgnoreCase("Tests"))
+               {
+                 testCaseNameList = Arrays.asList(entry.getValue().toString().split("\n"));
+               }
+            }
+            for(int i = 0; i < testCaseNameList.size(); i++)
+            {
+              String [] issueKey = testCaseNameList.get(i).toString().split(":");
+                testCaseIssueKeyList.add(issueKey[0]);
+            }
+            TestOutcome result = StepEventBus.getEventBus().getBaseStepListener().latestTestOutcome().get();
+            String executionFlag = SystemEnvironmentVariables.createEnvironmentVariables().getProperty("zephyr.executionFlag");
+            ZephyrCloudConnector zephyrCloudConnector = new ZephyrCloudConnector(testCaseIssueKeyList, result.isSuccess(), executionFlag);
+            Log.info("<--------End of marking the testcases status in Zephyr-------->");
         }
     }
 
